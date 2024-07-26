f99d1a8c071995d9b591be773403f18d838ea06f
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-07-26 06:29:39 +0300

    Trying to fix hosting issue10

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5RB6BXVI.mjs";import"./chunk-6VHRRWE5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-LXMULQPQ.mjs";import"./chunk-6VHRRWE5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
